5ef93483f942e8334f27350fdc56c33b160cdbc1
Rohit Gutal <rohitgutal@rohits-mbp.home>
2020-07-04 12:55:31 +0200

    functions practice

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,35 @@
-function sayHello(name) {
+// function sayHello(name) {
+//   console.log('Hi ' + name);
+// }
+
+const sayHello = (name='Rohit') => {
   console.log('Hi ' + name);
 }
 
+const sayHello1 = (greeting='Hi', name='Rohit') => {
+  console.log(`${greeting} ${name}`);
+}
+
+const sayHello2 = (name) => {
+  console.log(`Hello ${name}`);
+}
+
+const sayHello3 = () => 'Goededag Rohit!';
+
 sayHello();
+sayHello1('Hello', 'World');
+sayHello2();
+console.log(sayHello3());
+
+const checkInput = (callBackFn, ...unlimitedStrings) => {
+  for (const singleString of unlimitedStrings) {
+    if (singleString) {
+      callBackFn(singleString);
+    }
+  }
+}
+
+checkInput(sayHello, 'Rohit', 'Namrata', 'Bhagyashri', 'Hanumant');
+
+const bindFnBtnSelected = document.getElementById('bindFunctionsBtn');
+bindFnBtnSelected.addEventListener('click', sayHello2.bind(this, 'Max'))
