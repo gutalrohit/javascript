e1885b93ee0929f8e63c1d817966b92ef9b418ac
Rohit Gutal <rohitgutal@rohits-mbp.home>
2020-07-04 13:13:55 +0200

    dom selection queries

---
+++ section 6 - working with DOM/working-with-dom.js
@@ -0,0 +1,23 @@
+// selecting elements using different queryselectors
+// * when you select nodes using selectors, js objects containing properties of the elements are returned.
+// * you can only select element nodes, all the nodes are available on the document object
+
+console.dir(document.getElementById('item-3'));
+console.dir(document.getElementById('item-4')); //null
+console.dir(document.getElementsByClassName('item-class')); // array collection containg matching element nodes is returned
+console.log(document.getElementsByName('item')); // empty is returned
+console.log(document.getElementsByTagName('ul')); // empty is returned
+
+//Using css query selectors
+console.log(document.querySelector('#item-3'))
+console.log(document.querySelector('.item-class')) // forst element is returned
+console.log(document.querySelectorAll('.item-class')) // forst element is returned
+console.log(document.querySelector('ul li#item-3')) // forst element is returned
+
+//special queries
+console.log(document.body)
+console.log(document.head)
+console.log(document.documentElement) //returns entire document
+
+
+
